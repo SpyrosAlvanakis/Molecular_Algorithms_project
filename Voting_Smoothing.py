6a6f0362ddcf70ce5e32ad4f42ab540ca78b723c
Daphne <daphnetsolissou@gmail.com>
2023-07-10 12:18:02 +0300

    Add voting smoothing and extraction in final pipeline

--- Voting_Smoothing.py
+++ Voting_Smoothing.py
@@ -1,20 +1,31 @@
 import pandas as pd 
 import math
-import numpy as np
-import math 
+import sys
 
-def calculate_voting_results(concat):
+
+def select_algorithms_to_vote(grouped_df, algs):
+    mask_algs = grouped_df['Algorithm'].isin(algs)
+    grouped_df = grouped_df[mask_algs].copy()
+    grouped_df.reset_index(inplace=True, drop=True)
+    return grouped_df
+
+
+def calculate_voting_results(grouped_df):
+
+    grouped_df['Width'].fillna(grouped_df['Site'].str.len(), inplace=True)
+    grouped_df['Position_Range'] = grouped_df.apply(
+        lambda row: range(int(row['Starting_position']), int(row['Starting_position'] + row['Width'])), axis=1)
     # Make sure Position_Range contains list of positions, not range objects
-    concat['Position_Range'] = concat['Position_Range'].apply(list)
+    grouped_df['Position_Range'] = grouped_df['Position_Range'].apply(list)
 
     # Expand each list of positions into new rows, keeping the same index
-    positions = concat['Position_Range'].apply(pd.Series).stack()
+    positions = grouped_df['Position_Range'].apply(pd.Series).stack()
 
     # Reset index and rename columns for clarity
     positions = positions.reset_index().rename(columns={'level_0': 'original_index', 0: 'Position'})
 
     # Add Subgroup information back into new DataFrame
-    positions['Subgroup'] = positions['original_index'].apply(lambda idx: concat.loc[idx, 'Subgroup'])
+    positions['Subgroup'] = positions['original_index'].apply(lambda idx: grouped_df.loc[idx, 'Subgroup'])
 
     # Now, for each subgroup, we can count the occurrences of each position
     votes = positions.groupby(['Subgroup', 'Position']).size().reset_index().rename(columns={0: 'Votes'})
@@ -27,15 +38,16 @@ def calculate_voting_results(concat):
     
     return voting_results
 
-def apply_smoothing(concat_df, width_column, voting_df):
+
+def apply_smoothing(grouped_df, width_column, voting_df):
     # Calculate rounded width values
-    concat_df['Rounded_Width'] = concat_df[width_column].apply(lambda x: math.ceil(x / 2))
+    grouped_df['Rounded_Width'] = grouped_df[width_column].apply(lambda x: math.ceil(x / 2))
     
     # Transpose the DataFrame
     transposed_df = voting_df.transpose()
     
     # Apply rolling window operation row-wise
-    window_size = int(concat_df['Rounded_Width'].median())
+    window_size = int(grouped_df['Rounded_Width'].median())
     smoothed_df = transposed_df.rolling(window=window_size, center=True, min_periods=1).sum()
     
     # Transpose the result back to the original orientation
@@ -57,13 +69,14 @@ def get_max_and_range(smoothed_df):
     
     return max_values_df
 
-def counts_of_common(concat):
+
+def counts_of_common(grouped_df):
     # Create dictionaries to store the counts of sites and starting positions
     site_counts = {}
     starting_position_counts = {}
 
     # Iterate over each row in the concatenated dataframe
-    for index, row in concat.iterrows():
+    for index, row in grouped_df.iterrows():
         subgroup = row['Subgroup']
         site = row['Site']
         starting_position = row['Starting_position']
@@ -83,6 +96,6 @@ def counts_of_common(concat):
             if starting_position not in starting_position_counts[subgroup]:
                 starting_position_counts[subgroup][starting_position] = 0
             starting_position_counts[subgroup][starting_position] += 1
-    return  starting_position_counts, site_counts  
+    return starting_position_counts, site_counts
 
 
